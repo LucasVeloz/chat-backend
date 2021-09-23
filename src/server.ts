fd5b6105f3ffb32ad65cd5c8dde3ddad87d0ba44
LucasVeloz <lurucell@gmail.com>
2021-09-23 18:34:37 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
+
+interface Props {
+  id: string;
+  hour: Date;
+  message: string;
+}
+
+
+const app = express();
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
+});
+
+const chat: Props[] = [];
+const users: String[] = []
+
+
+io.on('connection', (socket) => {
+  socket.on('connected', data => {
+    socket.join('conversation');
+    if (!users.find(element => element === data)) {
+      users.push(data);
+    };
+  });
+
+  socket.on('message', data => {
+    if (users.find(element => element === data.id)) {
+      chat.push(data);
+      io.to('conversation').emit("chat", data);
+    }
+  })
+})
+
+
+
+serverHttp.listen(3333, () => console.log('Server is running! 🚀'))
