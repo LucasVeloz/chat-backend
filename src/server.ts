f42797a2d6eec0263508a0e9f1a3b953eef8e400
LucasVeloz <lurucell@gmail.com>
2022-03-13 16:38:58 -0300

    feat: adding new rooms

--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 3333;
 
 
 const app = express();
-
-app.get('/', (_, response) => response.send({
-  message: 'deu bom'
-}))
+app.use(express.json())
 
 const serverHttp = http.createServer(app);
 const io = new Server(serverHttp, {
@@ -27,12 +24,26 @@ const io = new Server(serverHttp, {
 
 const chat: Props[] = [];
 const users: String[] = []
+const rooms: String[] = [];
 
+
+app.get('/rooms', (_, response) => response.send(rooms))
+
+app.post('/rooms', (request, response) => {
+  const chat = request.body.chat;
+
+  if(rooms.find(item => item === chat)) {
+    response.status(400).send()
+  }
+  rooms.push(chat);
+
+  response.status(201).send()
+})
 
 io.on('connection', (socket) => {
   socket.on('connected', data => {
-    socket.join('conversation');
-    if (!users.find(element => element === data)) {
+    socket.join(data.room);
+    if (!users.find(element => element === data.user)) {
       users.push(data);
     };
   });
